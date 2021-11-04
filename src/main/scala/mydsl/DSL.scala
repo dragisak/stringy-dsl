ede1858274d9d564d5e588c3e6f43eacdba8f32c
Dragisa Krsmanovic <dragisa@crunchbase.com>
2021-11-04 13:47:53 -0700

    Type

--- src/main/scala/mydsl/DSL.scala
+++ src/main/scala/mydsl/DSL.scala
@@ -5,13 +5,12 @@ import NoWhitespace._
 
 object DSL {
 
-  sealed trait Expr
-  case class Num(value: Int)                      extends Expr
-  case class Str(value: String)                   extends Expr
-  case class Add(a: Expr, b: Expr)                extends Expr
-  sealed trait Bool                               extends Expr
-  case class Eq(a: Expr, b: Expr)                 extends Bool
-  case class IfElse[T](c: Bool, a: Expr, b: Expr) extends Expr
+  sealed trait Expr[T]
+  case class Num(value: Int)                                     extends Expr[Int]
+  case class Str(value: String)                                  extends Expr[String]
+  case class Add[T](a: Expr[T], b: Expr[T])                      extends Expr[T]
+  case class Eq[T](a: Expr[T], b: Expr[T])                       extends Expr[Boolean]
+  case class IfElse[T](c: Expr[Boolean], a: Expr[T], b: Expr[T]) extends Expr[T]
 
   def plus[_: P]: P[Unit]    = P("+")
   def equals[_: P]: P[Unit]  = P("==")
@@ -20,14 +19,14 @@ object DSL {
   def curlyL[_: P]: P[Unit]  = P("{")
   def curlyR[_: P]: P[Unit]  = P("}")
 
-  def number[_: P]: P[Expr] = P(CharIn("0-9").rep(1).!.map(s => Num(s.toInt)))
-  def string[_: P]: P[Expr] = P(("'" ~ AnyChar.rep.! ~ "'").map(Str))
+  def number[_: P]: P[Expr[Int]]    = P(CharIn("0-9").rep(1).!.map(s => Num(s.toInt)))
+  def string[_: P]: P[Expr[String]] = P(("'" ~ AnyChar.rep.! ~ "'").map(Str))
 
-  def add[_: P](p: => P[Expr]): P[Expr] = P((p ~ plus ~ p).map { case (a, b) => Add(a, b) })
+  def add[_: P, T](p: => P[Expr[T]]): P[Expr[T]] = P((p ~ plus ~ p).map { case (a, b) => Add(a, b) })
 
-  def eq[_: P, T](p: => P[Expr]): P[Bool] = P((p ~ equals ~ p).map { case (a, b) => Eq(a, b) })
+  def eq[_: P, T](p: => P[Expr[T]]): P[Expr[Boolean]] = P((p ~ equals ~ p).map { case (a, b) => Eq(a, b) })
 
-  def ifElse[_: P, T](p: => P[Expr]): P[Expr] =
+  def ifElse[_: P, T](p: => P[Expr[T]]): P[Expr[T]] =
     P(
       ("if" ~ parensL ~ eq(p) ~ parensR ~ curlyL ~ add(p) ~ curlyR ~ "else" ~ curlyL ~ add(p) ~ curlyR)
         .map { case (cond, whenTrue, whenFalse) =>
